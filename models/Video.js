41dacaea607c8b8b4d45b0d39576e930eda180d9
nongboyna2 <nongboyna2@gmail.com>
2025-04-14 19:00:50 +0700

    Update Video.js

--- models/Video.js
+++ models/Video.js
@@ -1,1 +1,31 @@
+const mongoose = require('mongoose');
 
+// Schema for Video
+const VideoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  file: {
+    type: String,
+    required: true, // File ID from Backblaze B2
+  },
+  subtitles: [
+    {
+      language: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+}, { timestamps: true });
+
+module.exports = mongoose.model('Video', VideoSchema);
